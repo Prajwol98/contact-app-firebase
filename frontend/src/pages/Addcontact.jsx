90b4b35c9f22eeafda7f85704279b803da19015c
Prajwol Mainali <prajwolmainali96@gmail.com>
2025-05-16 21:38:25 +0545

    update

---
+++ frontend/src/pages/Addcontact.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Addcontact = () => {
+  return <div>Addcontact</div>;
+};
+
+export default Addcontact;
---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import Navbar from "../components/Navbar";
+
+const Home = () => {
+  return (
+    <div>
+      <h1 className="bg-red-500">Contact app</h1>
+      <Navbar></Navbar>
+    </div>
+  );
+};
+
+export default Home;

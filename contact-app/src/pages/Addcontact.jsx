10145eae360a331423b59ca0e204bce4756d1260
Prajwol Mainali <prajwolmainali96@gmail.com>
2025-05-16 21:40:09 +0545

    update

--- contact-app/src/pages/Addcontact.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Addcontact = () => {
-  return <div>Addcontact</div>;
-};
-
-export default Addcontact;
--- contact-app/src/pages/Editcontact.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Editcontact = () => {
-  return <div>Editcontact</div>;
-};
-
-export default Editcontact;
